06d9c861f1af54f5144315328b285d410a22abef
Chris Palmer <capalmer1013@gmail.com>
2019-10-07 19:00:39 -0400

    added socketio tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,8 +1,17 @@
+from flask_socketio import SocketIO
 from netbeez import app, models
 from .test import BaseTest
+
+socketio = SocketIO(app)
 
 
 class TestDefault(BaseTest):
     def test_index(self):
         result = self.app.get("/")
         assert result.status_code == 200
+
+
+class TestSocketIO(BaseTest):
+    def test_connect(self):
+        client = socketio.test_client(app)
+        pass
